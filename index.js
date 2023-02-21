8afec635ba3e17f803deaead1a794e78ab7ed14c
Shawn Tseng <shawntseng40@gmail.com>
2023-02-21 21:25:09 +0800

    1

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 setInterval(() => {
-    document.title = new Date();
+    var time = new Date();
+    document.title = time;
+    updateURLParameter(window.location.href, 'datetime', time);
+    // newURL = updateURLParameter(newURL, 'resId', 'newResId');
+    // window.history.replaceState('', '', updateURLParameter(window.location.href, "param", "value"));
 }, 1000);
+
+
+/**
+ * http://stackoverflow.com/a/10997390/11236
+ */
+function updateURLParameter(url, param, paramVal) {
+    var TheAnchor = null;
+    var newAdditionalURL = "";
+    var tempArray = url.split("?");
+    var baseURL = tempArray[0];
+    var additionalURL = tempArray[1];
+    var temp = "";
+
+    if (additionalURL) {
+        var tmpAnchor = additionalURL.split("#");
+        var TheParams = tmpAnchor[0];
+        TheAnchor = tmpAnchor[1];
+        if (TheAnchor)
+            additionalURL = TheParams;
+
+        tempArray = additionalURL.split("&");
+
+        for (var i = 0; i < tempArray.length; i++) {
+            if (tempArray[i].split('=')[0] != param) {
+                newAdditionalURL += temp + tempArray[i];
+                temp = "&";
+            }
+        }
+    }
+    else {
+        var tmpAnchor = baseURL.split("#");
+        var TheParams = tmpAnchor[0];
+        TheAnchor = tmpAnchor[1];
+
+        if (TheParams)
+            baseURL = TheParams;
+    }
+
+    if (TheAnchor)
+        paramVal += "#" + TheAnchor;
+
+    var rows_txt = temp + "" + param + "=" + paramVal;
+    return baseURL + "?" + newAdditionalURL + rows_txt;
+}
